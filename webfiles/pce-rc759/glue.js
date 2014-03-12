5255967195319e7c9b05d2cda1f74bb14a4afafa
marciot <mlt4356-github@yahoo.com>
2014-03-11 20:30:47 -0600

    Added Piccoline RC759 support

---
+++ webfiles/pce-rc759/glue.js
@@ -0,0 +1,39 @@
+/*
+RetroWeb Browser
+Copyright (C) 2014 Marcio Teixeira
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+function emulatorPreRun() {
+	Module.arguments = ["-c", "roms/pce-config.cfg", "-r"];
+}
+
+// Loads a disk that has been copied to the emscripten local store
+function emulatorMountDisk(disk) {
+	console.log("Mounting " + disk);
+	FS.rename(disk,"fd0.st");
+	rcSetMessage ("emu.disk.insert", "0:fd0.st");
+}
+
+function emulatorReset() {
+	rcSetMessage ("emu.reset", "");
+}
+
+function rcSetMessage(msg,val) {
+	var sim = _rc759_get_sim();
+	var _rcSetMessage = Module.cwrap('rc759_set_msg', 'int', ['int','string', 'string']);
+	_rcSetMessage(sim, msg, val);
+}
