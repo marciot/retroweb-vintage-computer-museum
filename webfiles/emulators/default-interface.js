f82b82bd415128ef2e97e447185101448d14e42e
Marcio T <mlt4356-github@yahoo.com>
2016-06-24 13:27:17 -0600

    Converted EmulatorInterface to ECMA 6 class

--- webfiles/emulators/default-interface.js
+++ webfiles/emulators/default-interface.js
@@ -19,70 +19,65 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 /* Reference:
  *   http://joshgertzen.com/object-oriented-super-class-method-calling-with-javascript/
- *
- * I am using "Static ( Hard Coded ) Super Calling That Works"
  */
 
-function createDefaultEmulatorInterface() {
-	var ifce = new EmulatorInterface();
-	emuState.setEmulatorInterface(ifce);
-	return ifce;
-}
-
-function EmulatorInterface() {
-	this.arguments = {};
-}
-
-EmulatorInterface.prototype.getFileNameForDrive = function(drive, fileName) {
-	if( typeof this.getDrives == "function" ) {
-		var fileName = this.getDrives()[drive];
-		return fileName;
-	} else {
-		return drive + ".disk";
+class EmulatorInterface {
+	constructor() {
+		this.arguments = {};
+		emuState.setEmulatorInterface(this);
 	}
-}
 
-EmulatorInterface.prototype.setArgument = function(arg, value) {
-	this.arguments[arg] = value;
-	console.log("Setting argument " + arg + " to " + value);
-}
-
-EmulatorInterface.prototype.configModule = function(module) {
-	module.arguments = [];
-	for (var arg in this.arguments) {
-		module.arguments.push(arg);
-		if(this.arguments[arg] != '') {
-			module.arguments.push(this.arguments[arg]);
+	getFileNameForDrive(drive, fileName) {
+		if( typeof this.getDrives == "function" ) {
+			var fileName = this.getDrives()[drive];
+			return fileName;
+		} else {
+			return drive + ".disk";
 		}
 	}
-}
 
-EmulatorInterface.prototype.preInit = function() {
-}
-
-EmulatorInterface.prototype.preRun = function() {
-}
-	
-EmulatorInterface.prototype.prepareDisk = function(diskFile) {
-}
-
-EmulatorInterface.prototype.mountDisk = function(diskFile) {
-	alert( "Disk insertion after boot is not available for this emulator. Reload the page to reset the emulator." );
-}
-
-EmulatorInterface.prototype.reset = function() {
-	alert( "This functionality is not available for this emulator. Refresh the page to restart the emulator." );
-}
-	
-EmulatorInterface.prototype.bootFromRom = function() {
-	if(emuState.isRunning()) {
-		alert("Cannot change the boot media once the computer has already restarted. Please reload the page to reset");
-	} else {
-		emuState.bootMediaLoaded();
-		emuState.requestRestart();
+	setArgument(arg, value) {
+		this.arguments[arg] = value;
+		console.log("Setting argument " + arg + " to " + value);
 	}
-}
 
-EmulatorInterface.prototype.cassetteAction = function(action) {
-	alert( "This emulator does not support the cassette interface." );
+	configModule(module) {
+		module.arguments = [];
+		for (var arg in this.arguments) {
+			module.arguments.push(arg);
+			if(this.arguments[arg] != '') {
+				module.arguments.push(this.arguments[arg]);
+			}
+		}
+	}
+
+	preInit() {
+	}
+
+	preRun() {
+	}
+	
+	prepareDisk(diskFile) {
+	}
+
+	mountDisk(diskFile) {
+		alert( "Disk insertion after boot is not available for this emulator. Reload the page to reset the emulator." );
+	}
+
+	reset() {
+		alert( "This functionality is not available for this emulator. Refresh the page to restart the emulator." );
+	}
+	
+	bootFromRom() {
+		if(emuState.isRunning()) {
+			alert("Cannot change the boot media once the computer has already restarted. Please reload the page to reset");
+		} else {
+			emuState.bootMediaLoaded();
+			emuState.requestRestart();
+		}
+	}
+
+	cassetteAction(action) {
+		alert( "This emulator does not support the cassette interface." );
+	}
 }
