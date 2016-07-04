4db24b0bf2919b3837ae46f917c2cbaff68562ce
Marcio T <mlt4356-github@yahoo.com>
2016-07-04 13:02:10 -0600

    Upgraded to jQuery 3.0

--- webfiles/lib/bootstrap.js
+++ webfiles/lib/bootstrap.js
@@ -64,7 +64,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 if(!(RetroWeb.query.debug == "raw")) {
 	//document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
-	document.write('<script type="text/javascript" src="http://code.jquery.com/jquery-1.12.4.min.js"></script>');
+	document.write('<script type="text/javascript" src="https://code.jquery.com/jquery-3.0.0.min.js"></script>');
 	document.write('<script type="text/javascript" src="/lib/webcomponents/webcomponents.min.js"></script>');
 	document.write('<link rel="import" href="/components/inline-svg.html"></link>');
 	document.write('<link rel="import" href="/components/animated-zoom.html"></link>');
